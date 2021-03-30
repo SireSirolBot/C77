27e721edc5707405439a08bc69863b021d096ae9
Python <80469347+SireSirolBot@users.noreply.github.com>
2021-03-30 17:51:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("My First Console Project")
+var name1 = "Ranbir "
+var name2 = "Kapoor"
+console.log(name1 + name2)
+var fullname = name1.concat(name2)
+console.log(fullname)
+var a = 10;
+var b = 20;
+var add = a+b
+console.log(add)
+var sub = a-b
+console.log(sub)
+var multi = a*b
+console.log(multi)
+var divi = a/b
+console.log(divi)
